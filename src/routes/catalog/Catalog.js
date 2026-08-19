06041dbcd72e0f83b8ee976302d6db6693bee1c6
Carlos Ibarra <carlosibarra.art@gmail.com>
2026-08-18 21:52:54 -0700

    Testing catalog page audio feature once again.

--- src/routes/catalog/Catalog.js
+++ src/routes/catalog/Catalog.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { directory } from '../../pages/directory/directory';
 import Dropdown from '../../components/dropdown/Dropdown';
